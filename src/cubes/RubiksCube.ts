35fd1da20af97b97aaebc9457bb2548ab61e8923
dwuggh <dwuggh@gmail.com>
2020-05-01 23:01:48 +0800

    format code, basic rubiks cube structure & show

--- src/cubes/RubiksCube.ts
+++ src/cubes/RubiksCube.ts
@@ -1,5 +1,12 @@
 import * as THREE from 'three'
-
+import { TaskRunner } from './Scenario'
+declare module '*.png'
+import * as BLUE from '../assets/cubes/blue.png'
+import * as GREEN from '../assets/cubes/green.png'
+import * as ORANGE from '../assets/cubes/orange.png'
+import * as RED from '../assets/cubes/red.png'
+import * as WHITE from '../assets/cubes/white.png'
+import * as YELLOW from '../assets/cubes/yellow.png'
 /*
   the interface to load all three-created regular cube models
   some types of cube's rotation layer can transfrom between moves, which cannot be described by the following order set.
@@ -8,49 +15,161 @@ type state = number[]
 type stateMap = [state, state]
 type order = [string, stateMap]
 interface RegularModel {
-    meshs: THREE.Mesh[]
-    orderSet: order[]
+  meshs: THREE.Mesh[]
+  orderSet: order[]
 }
 
+class MyCubeMaterial {
+  materials: THREE.Material[]
+  constructor() {
+    this.materials = [
+      new THREE.MeshBasicMaterial({ color: 0x00ffff }),
+      new THREE.MeshBasicMaterial(),
+      new THREE.MeshBasicMaterial(),
+      new THREE.MeshBasicMaterial(),
+      new THREE.MeshBasicMaterial(),
+      new THREE.MeshBasicMaterial(),
+    ]
+    this.materials.forEach((el) => {
+      el.needsUpdate = true
+      el.setValues({
+        opacity: 0,
+        transparent: true,
+        side: THREE.DoubleSide,
+      })
+    })
+  }
+}
 /*
   create a N layer rubik's cube.
 */
 
 export class RubiksCube implements RegularModel {
-    layer: number
-    meshs: THREE.Mesh[]
-    orderSet: order[]
-    state: state
-    flag: boolean //flag for whether rotating or not
+  scene: THREE.Scene
+  layer: number
+  meshs: THREE.Mesh[]
+  group: THREE.Group
+  cubeMat: THREE.Mesh[][][]
+  orderSet: order[]
+  state: state
+  taskRunner: TaskRunner
+  flag: boolean //flag for whether rotating or not
 
-    constructor(layer: number) {
-        this.layer = layer
-        this.meshs = this._generateMeshs()
-        this.orderSet = this._generateOrderSet()
-        this.flag = false
+  constructor(layer: number, scene: THREE.Scene, taskRunner: TaskRunner) {
+    this.layer = layer
+    this.scene = scene
+    this.meshs = []
+    this.cubeMat = []
+    for (let i = 0; i < layer; i++) {
+      this.cubeMat.push([])
+      for (let j = 0; j < layer; j++) {
+        this.cubeMat[i].push(new Array(3))
+      }
+    }
+    this.group = new THREE.Group()
+    this.group.scale.setScalar(0.2)
+    this.group.setRotationFromEuler(new THREE.Euler(0.4, 1, 0, 'XYZ'))
+    this._generateMeshs()
+    // this.orderSet = this._generateOrderSet()
+    this.flag = false
 
-        // set current state to standard state
-        this.state = new Array(layer * layer * layer)
-        this.state.forEach((val, index, arr) => {
-            val = index
-        })
+    this.scene.add(this.group)
+
+    // set current state to standard state
+    this.state = new Array(layer * layer * layer)
+    this.state.forEach((val, index) => {
+      val = index
+    })
+
+    this.taskRunner = taskRunner
+  }
+
+  private _generateMeshs() {
+    const geometry = new THREE.BoxGeometry()
+    for (let i = 0; i < this.layer; i++) {
+      for (let j = 0; j < this.layer; j++) {
+        for (let k = 0; k < this.layer; k++) {
+          const cubeMesh = new THREE.Mesh(
+            geometry,
+            new MyCubeMaterial().materials
+          )
+          cubeMesh.name = (
+            i * this.layer * this.layer +
+            j * this.layer +
+            k
+          ).toString()
+          this.cubeMat[i][j][k] = cubeMesh
+          this.meshs[
+            i * this.layer * this.layer + j * this.layer + k
+          ] = cubeMesh
+          cubeMesh.position.set(i - 1, j - 1, k - 1)
+          this.group.add(cubeMesh)
+        }
+      }
     }
 
-    public performOrder(ord: order): void {}
-    public performOrderList(ords: order[]): void {}
-
-    // randomly scramble this cube and return afterward state
-    public scramble(): state {}
-
-    // solve a cube with given/current state, return orders list
-    public static solve(layer, state: state): order[]
-    public solve(): order[] {
-        return RubiksCube.solve(this.layer, this.state)
+    for (let i = 0; i < this.layer; i++) {
+      for (let j = 0; j < this.layer; j++) {
+        this.cubeMat[i][j][0].material[5].setValues(this._loadStickers(5))
+        this.cubeMat[i][j][2].material[4].setValues(this._loadStickers(4))
+        this.cubeMat[i][0][j].material[3].setValues(this._loadStickers(3))
+        this.cubeMat[i][2][j].material[2].setValues(this._loadStickers(2))
+        this.cubeMat[0][i][j].material[1].setValues(this._loadStickers(1))
+        this.cubeMat[2][i][j].material[0].setValues(this._loadStickers(0))
+        console.log(this.cubeMat[i][j][2])
+      }
     }
+  }
 
-    private _generateMeshs(): THREE.Mesh[] {}
+  private _generateMaterial(): THREE.Material[] {
+    const materials = [
+      new THREE.MeshBasicMaterial(),
+      new THREE.MeshBasicMaterial(),
+      new THREE.MeshBasicMaterial(),
+      new THREE.MeshBasicMaterial(),
+      new THREE.MeshBasicMaterial(),
+      new THREE.MeshBasicMaterial(),
+    ]
+    materials.forEach((el) => {
+      el.needsUpdate = true
+      el.setValues({
+        opacity: 0,
+        transparent: true,
+        side: THREE.DoubleSide,
+      })
+    })
+    return materials
+  }
 
-    private _generateOrder(): order {}
+  private _loadStickers(color = 6) {
+    const stickers = [BLUE, GREEN, WHITE, ORANGE, RED, YELLOW]
 
-    private _generateOrderSet(): order[] {}
+    const stickerSetting = {
+      transparent: false,
+      map:
+        color == 6
+          ? null
+          : new THREE.TextureLoader().load(
+              // '../assets/cubes/' + stickers[color] + '.png'
+              stickers[color]
+            ),
+    }
+    console.log('../assets/cubes/' + stickers[color] + '.svg')
+    return stickerSetting
+  }
+
+  // public performOrder(ord: order): void {}
+  // public performOrderList(ords: order[]): void {}
+
+  // // randomly scramble this cube and return afterward state
+  // public scramble(): state {}
+
+  // // solve a cube with given/current state, return orders list
+  // public static solve(layer, state: state): order[]
+  // public solve(): order[] {
+  //   return RubiksCube.solve(this.layer, this.state)
+  // }
+  // private _generateOrder(): order {}
+
+  // private _generateOrderSet(): order[] {}
 }
--- src/cubes/Scenario.ts
+++ src/cubes/Scenario.ts
@@ -1,43 +1,92 @@
 import * as THREE from 'three'
 
-export default class Scenario {
-    width: number
-    height: number
-    scene: THREE.Scene
-    camera: THREE.PerspectiveCamera
-    renderer: THREE.Renderer
+// https://www.cnblogs.com/Wayou/p/typescript_task_runner.html
+type Task<T> = {
+  name: string
+  func: () => Promise<T>
+}
 
-    constructor(element: HTMLCanvasElement, width: number, height: number) {
-        // this.cube = document.getElementById(element).parentElement;
-        // console.log(this.cube);
-        this.width = width
-        this.height = height
-        this.scene = new THREE.Scene()
-        this.camera = new THREE.PerspectiveCamera(25, width / height, 0.1, 1000)
-        this.renderer = new THREE.WebGLRenderer({
-            antialias: true,
-            canvas: element,
-        })
-        this.camera.position.set(0, 0, 5)
-        this.renderer.setSize(width, height)
+export class TaskRunner {
+  private _queue: Task<any>[] = []
+  private activeTaskNum = 0
 
-        this.scene.add(new THREE.AxesHelper(10))
-        this.animate()
+  constructor(private limit = 5, public debug = false) {
+    if (limit < 1) {
+      throw new Error('limit < 1')
+    }
+  }
 
-        // window.addEventListener("resize", this.onWindowResize, false);
+  public addTask<T>(task: Task<T>) {
+    this._queue.push(task)
+    this.activeTaskNum++
+    if (this.activeTaskNum == 1)
+      this._runTask()
+  }
+
+  private _runTask() {
+    while (this.activeTaskNum < this.limit && this._queue.length > 0) {
+      const task = this._queue.shift()
+      this._excute(task)
     }
-    public onWindowResize = () => {
-        this.width = window.innerWidth * 0.8
-        this.height = window.innerHeight * 0.8
-        this.camera.aspect = this.width / this.height
-        this.camera.updateMatrix()
-        this.renderer.setSize(this.width, this.height)
-    }
-    private animate = () => {
-        requestAnimationFrame(this.animate)
-        this.render()
-    }
-    private render() {
-        this.renderer.render(this.scene, this.camera)
-    }
+  }
+
+  private _excute<T>(task: Task<T>) {
+    return task
+      .func()
+      .then((result) => {
+        return result
+      })
+      .catch((reason) => {
+        // do sth
+      })
+      .finally(() => {
+        this.activeTaskNum--
+        this._runTask()
+      })
+  }
+}
+
+export class Scenario {
+  width: number
+  height: number
+  scene: THREE.Scene
+  camera: THREE.PerspectiveCamera
+  renderer: THREE.Renderer
+  taskRunner: TaskRunner
+
+  constructor(element: HTMLCanvasElement, width: number, height: number) {
+    // this.cube = document.getElementById(element).parentElement;
+    // console.log(this.cube);
+    this.width = width
+    this.height = height
+    this.scene = new THREE.Scene()
+    this.scene.background = new THREE.Color(0xFEFFA8)
+    this.camera = new THREE.PerspectiveCamera(25, width / height, 0.1, 1000)
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: element,
+    })
+    this.camera.position.set(0, 0, 5)
+    this.renderer.setSize(width, height)
+
+    this.scene.add(new THREE.AxesHelper(10))
+
+    this.taskRunner = new TaskRunner(5, false)
+
+    // window.addEventListener("resize", this.onWindowResize, false);
+  }
+  public onWindowResize = () => {
+    this.width = window.innerWidth * 0.8
+    this.height = window.innerHeight * 0.8
+    this.camera.aspect = this.width / this.height
+    this.camera.updateMatrix()
+    this.renderer.setSize(this.width, this.height)
+  }
+  public animate = () => {
+    requestAnimationFrame(this.animate)
+    this.render()
+  }
+  private render() {
+    this.renderer.render(this.scene, this.camera)
+  }
 }
